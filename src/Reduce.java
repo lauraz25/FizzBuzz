ee562f9b11e3e693081f1162e07c8ce9762c8e5a
lauraz25 <laurazhan21@gmail.com>
2023-09-11 23:31:05 -0400

    completed

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+
+        while (n > 0){
+            if (n % 2 == 0){
+                n = n / 2;
+            }
+            else{
+                n = n - 1;
+            }
+
+            count++;
+        }
+
+        System.out.println("It took " + count + " steps to get to 0 from 100.");
+    }
 }
